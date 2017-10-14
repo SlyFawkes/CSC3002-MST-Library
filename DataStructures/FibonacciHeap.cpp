79f1880ded0a44d7d7f4994809d44ef90cc35bfc
Dean Welch <dwelch02@qub.ac.uk>
2017-10-14 23:17:34 +0100

    add node to root list

--- DataStructures/FibonacciHeap.cpp
+++ DataStructures/FibonacciHeap.cpp
@@ -12,9 +12,14 @@ void FibonacciHeap::insert(Node* node) {
     node->child = nullptr;
     node->marked = false;
     if (minimumNode == nullptr) {
+        //TODO possibly remove?
+        node->left = nullptr;
+        node->right = nullptr;
         minimumNode = node;
     } else {
         if (node->key < minimumNode->key) {
+            node->left = minimumNode;
+            node->right = minimumNode->right;
             minimumNode = node;
         }
     }
